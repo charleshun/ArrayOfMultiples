10af6605330775ad8aaad4b95d6720a3904333d3
RBS <ronai.balint@naih.hu>
2022-04-11 11:30:43 +0200

    init

---
+++ Prg/Prgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Prg
+{
+    public class Prgs
+    {
+	public static int[] ArrayOfMultiples(int n, int length) {
+		return Enumerable.Range(n, length)
+            .Select(n=>n*n)
+            .ToArray();
+	}
+    }
+}
